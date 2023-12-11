6a02811a1cb467effbf5a7fea80122644a2d31c4
Henry Voigt <oigty69@freenet.de>
2023-12-11 22:51:37 +0100

    update ExpensesDataHandler

--- src/App/src/Handler/ExpensesDataHandler.php
+++ src/App/src/Handler/ExpensesDataHandler.php
@@ -48,4 +48,51 @@ class ExpensesDataHandler implements RequestHandlerInterface
         $this->expensesDataService = $expensesDataService;
         $this->utilitiesService = $utilitiesService;
     }
+
+    public function getExpensesPeriodAction(ServerRequestInterface $request): ResponseInterface
+    {
+       
+        $params = $this->getParameter($request);
+           
+        $check = $this->utilitiesService->checkWhetherParameterExistAndIsString($params, 'from');
+        if($check != null) {
+            return $check;
+        }
+
+        $check = $this->utilitiesService->checkWhetherParameterExistAndIsString($params, 'to');
+        if($check != null) {
+            return $check;
+        }
+
+        $serviceResult = $this->expensesDataService->fetchExpensesPeriod($params['from'], $params['to']);
+        $error = $this->utilitiesService->checkServiceErrorForResponse($serviceResult);
+        return $error == null ? new JsonResponse($serviceResult) : $error;
+    }
+    
+    public function putExpensesAction(ServerRequestInterface $request): ResponseInterface
+    {
+       
+        $userId = $request->getAttribute("userId");
+
+        $params = $this->getParameter($request);
+           
+        $check = $this->utilitiesService->checkWhetherParameterExistAndIsNumeric($params, 'categoryCompositionId');
+        if($check != null) {
+            return $check;
+        }
+
+        $check = $this->utilitiesService->checkWhetherParameterExistAndIsNumeric($params, 'price');
+        if($check != null) {
+            return $check;
+        }
+        
+        $check = $this->utilitiesService->checkWhetherParameterExistAndIsString($params, 'metatext');
+        if($check != null) {
+            return $check;
+        }
+
+        $serviceResult = $this->expensesDataService->insertExpenses($userId, $params['categoryCompositionId'], $params['price'], $params['metatext']);
+        $error = $this->utilitiesService->checkServiceErrorForResponse($serviceResult);
+        return $error == null ? new JsonResponse($serviceResult) : $error;
+    }
 }
