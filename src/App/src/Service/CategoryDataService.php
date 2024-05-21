edc9d9f7ba4fae96651880c0e5d9087a774ad2dc
Henry Voigt <oigty69@freenet.de>
2024-05-22 00:17:30 +0200

    refactoring category data service

--- src/App/src/Service/CategoryDataService.php
+++ src/App/src/Service/CategoryDataService.php
@@ -33,7 +33,6 @@ class CategoryDataService
         LoggerInterface $logger
     )
     {
-
         $this->config = $config;
         $this->logger = $logger;
         $this->db = $db;
@@ -41,24 +40,25 @@ class CategoryDataService
 
     public function getCategories()
     {
+        $categoriesDataQuery = <<<SQLQUERY
+        SELECT id, title FROM categories ORDER BY id;
+        SQLQUERY;
+
+        $categoryCompositionsQuery = <<<SQLQUERY
+        SELECT id FROM category_compositions;
+        SQLQUERY;
+
+        $categoryCompositionsForMemberQuery = <<<SQLQUERY
+        SELECT c.title as category FROM category_composition_members ccm LEFT JOIN categories c ON ccm.categoryId = c.id WHERE ccm.categoryCompositionId = ?
+        SQLQUERY;
+
         try {
-
-            $categoriesData = $this->db->query(
-                "SELECT id, title FROM ".$this->config["tableNames"]["categories"]." ORDER BY id;",
-                Adapter::QUERY_MODE_EXECUTE
-            )->toArray();
-            
-            $categoryCompositions = $this->db->query("SELECT id FROM ".$this->config["tableNames"]["category_compositions"].";", Adapter::QUERY_MODE_EXECUTE)->toArray();
-
+            $categoriesData = $this->db->query($categoriesDataQuery, Adapter::QUERY_MODE_EXECUTE)->toArray();
+            $categoryCompositions = $this->db->query($categoryCompositionsQuery, Adapter::QUERY_MODE_EXECUTE)->toArray();
             $categoryCompositionsData = [];
 
             foreach ($categoryCompositions as $categoryCompositionsMember) {
-                $categoryCompositionsForMember = $this->db->query(
-                    "SELECT b.title as category FROM ".$this->config["tableNames"]["category_composition_members"]." a LEFT JOIN ".$this->config["tableNames"]["categories"]." b ON a.categoryId = b.id WHERE a.categoryCompositionId = ?;",
-                    [
-                        $categoryCompositionsMember["id"]
-                    ]
-                )->toArray();
+                $categoryCompositionsForMember = $this->db->query( $categoryCompositionsForMemberQuery, [$categoryCompositionsMember["id"]])->toArray();
                 
                 if(count($categoryCompositionsForMember) > 0) {
                     $categories = [];
@@ -91,14 +91,12 @@ class CategoryDataService
 
     public function storeCategory($userId, $title)
     {
+        $insertCategoryQuery = <<<SQLQUERY
+        INSERT INTO categories (title, created, userId) VALUES (?, now(), ?);
+        SQLQUERY;
+
         try {
-            $this->db->query(
-                "INSERT INTO ".$this->config["tableNames"]["categories"]." (title, created, userId) VALUES (?, now(), ?);",
-                [
-                    $title,
-                    $userId
-                ]
-            );
+            $this->db->query($insertCategoryQuery, [$title, $userId]);
             return true;
         } catch (Exception $e) {
             return [
@@ -110,48 +108,38 @@ class CategoryDataService
 
     public function storeCategoryComposition($userId, $categoryIds)
     {
+        $insertCategoryCompositionsQuery = <<<SQLQUERY
+        INSERT INTO category_compositions (created, userId) VALUES (now(), ?);
+        SQLQUERY;
+
+        $insertCategoryCompositionMembersQuery = <<<SQLQUERY
+        INSERT INTO category_composition_members (categoryId, categoryCompositionId ) VALUES (?, ?);
+        SQLQUERY;
+
         if(count($categoryIds) == 0) {
             $ex = new ArrayContentException("no categories in request found!");
             throw $ex;
         }
         
-        $connection = $this->db->getDriver()->getConnection();
-        $connection->beginTransaction();
-
+        $this->db->getDriver()->getConnection()->beginTransaction();
+       
         try {
-            $this->db->query(
-                "INSERT INTO ".$this->config["tableNames"]["category_compositions"]." (created, userId) VALUES (now(), ?);",
-                [
-                    $userId
-                ]
-            );
+            $this->db->query($insertCategoryCompositionsQuery, [$userId]);
             
-            $categoryCompositionIdResult = $this->db->query(
-                "SELECT LAST_INSERT_ID() AS lastCategoryCompositionId;",
-                Adapter::QUERY_MODE_EXECUTE
-            )->toArray();
-            
+            $categoryCompositionIdResult = $this->db->query("SELECT LAST_INSERT_ID() AS lastCategoryCompositionId;", Adapter::QUERY_MODE_EXECUTE)->toArray();
             $categoryCompositionId = $categoryCompositionIdResult[0]["lastCategoryCompositionId"];
     
-    
             foreach ($categoryIds as $categoryId) {
-                $this->db->query(
-                    "INSERT INTO ".$this->config["tableNames"]["category_composition_members"]." (categoryId, categoryCompositionId ) VALUES (?, ?);",
-                    [
-                        $categoryId,
-                        $categoryCompositionId,
-                    ]
-                );
+                $this->db->query($insertCategoryCompositionMembersQuery, [$categoryId, $categoryCompositionId]);
             }
+            $this->db->getDriver()->getConnection()->commit();
+            return true;
         } catch (Exception $e) {
-            $connection->rollback();
+            $this->db->getDriver()->getConnection()->rollback();
             return [
                 "error" => 1,
                 "errormsg" => $e->getMessage(),
             ];
         }
-       
-        $connection->commit();
-        return true;
     }
 }
