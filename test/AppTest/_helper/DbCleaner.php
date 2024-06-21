d98a782b3e80f5aeaa73dee5a1d549cb4d63c978
Henry Voigt <oigty69@freenet.de>
2024-06-21 16:14:18 +0200

    fix DbCleaner for unit tests

--- test/AppTest/_helper/DbCleaner.php
+++ test/AppTest/_helper/DbCleaner.php
@@ -1,19 +1,21 @@
 <?php
 
 use Laminas\Db\Adapter\Adapter;
+use App\Service\BackendServiceService;
 
-return function($config, $dbAdapter) {
-    
+return function($config, $dbAdapter, $logger) {
+    $backendServiceService = new BackendServiceService($config, $dbAdapter, $logger);
     try {
+        $sqlTables = $backendServiceService->getSqlTables();
 
         $connection = $dbAdapter->getDriver()->getConnection();
         $connection->beginTransaction();
 
-        foreach(array_reverse($this->config["tableNames"]) as $table) {
+        foreach(array_reverse($sqlTables) as $table) {
             $dbAdapter->query("DELETE FROM ".$table, Adapter::QUERY_MODE_EXECUTE);
         }
 
-        foreach(array_reverse($this->config["tableNames"]) as $table) {
+        foreach(array_reverse($sqlTables) as $table) {
             $dbAdapter->query("DROP TABLE IF EXISTS ".$table, Adapter::QUERY_MODE_EXECUTE);
         }
 
